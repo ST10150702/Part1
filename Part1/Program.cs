13629ba98736814d9b73af2dfcd54a97f25e01b3
ST10150702 <112873331+ST10150702@users.noreply.github.com>
2022-09-12 12:52:13 +0200

    Worked more on Part1 almost on Q2.4

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -8,7 +8,7 @@ namespace Part1
     //Abstract class Tile
     public abstract class Tile
     {
-        static int TileX, TileY;
+        public static int TileX, TileY;
         string sTileType;
 
         public enum TileType
@@ -16,12 +16,14 @@ namespace Part1
             Hero,Enemy,Gold,Weapon
         }
 
+        //Initiate Class
         public static void Initiate(int X, int Y)
         {
             TileX = X;
             TileY = Y;
         }
 
+        //ObstacleClass
         public class ObstacleClass
         {
             public string Obstacle(int posX, int posY)
@@ -32,7 +34,7 @@ namespace Part1
 
             }
         }
-
+        //EmptyTileClass
         public class EmptyTileClass
         {
             public string EmptyTile(int posX, int posY)
@@ -44,7 +46,82 @@ namespace Part1
 
     }
 
-    
+    //Character Abstract Class
+    public abstract class Character
+    {
+
+        float HP, Damage;
+        int MaxHP;
+
+        string[] Vision = { "North", "South", "East", "West" };
+
+        //Movement Enum
+        public enum Movement
+        {
+            NoMovement, Up, Down, Left, Right
+        }
+
+        //Attack Method
+        public virtual void Attack(Character target)
+        {
+            target.HP = HP - Damage; 
+        }
+
+        //isDead Method
+        public bool isDead(float HP)
+        {
+            if (HP == 0)
+            {
+                return true;
+            }
+            else
+                return false;  
+        }
+
+        //CheckRange Method
+        public virtual bool CheckRange(Character target)
+        {
+            if (DistanceTo(target) == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        //DistanceTo Method
+        private int DistanceTo(Character target)
+        {
+            
+        }
+
+        //Movement Method
+        public void Move(Movement Move)
+        {
+            if (Move == Movement.Up)
+            {
+                Tile.TileY = Tile.TileY + 1;
+            }
+            else if (Move == Movement.Down)
+            {
+                Tile.TileY = Tile.TileY - 1;
+            }
+            else if (Move == Movement.Left)
+            {
+                Tile.TileX = Tile.TileX - 1;
+            }
+            else if (Move == Movement.Right)
+            {
+                Tile.TileX = Tile.TileX + 1;
+            }
+        }
+
+        public Movement ReturnMove(Movement Move = 0)
+        {
+           
+        }
+
+    }
 
 
 
