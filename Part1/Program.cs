deec065695cd8831b42d98dd30f8f38a1f2cedb6
ST10150702 <112873331+ST10150702@users.noreply.github.com>
2022-09-12 11:39:33 +0200

    Added Part1 File (Not Completed)

---
+++ Part1/Program.cs
@@ -0,0 +1,52 @@
+﻿//Game Dev Project
+//Part 1
+
+using System;
+
+namespace Part1
+{
+    //Abstract class Tile
+    public abstract class Tile
+    {
+        static int TileX, TileY;
+        string sTileType;
+
+        public enum TileType
+        {
+            Hero,Enemy,Gold,Weapon
+        }
+
+        public static void Initiate(int X, int Y)
+        {
+            TileX = X;
+            TileY = Y;
+        }
+
+        public class ObstacleClass
+        {
+            public string Obstacle(int posX, int posY)
+            {
+
+                Tile.Initiate(posX, posY);
+                return ("Obstacle");
+
+            }
+        }
+
+        public class EmptyTileClass
+        {
+            public string EmptyTile(int posX, int posY)
+            {
+                Tile.Initiate(posX, posY);
+                return ("Empty Tile");
+            }
+        }
+
+    }
+
+    
+
+
+
+}
+
